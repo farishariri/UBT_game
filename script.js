5fb179fea0d55571d8d6c7ec8893a2d33c55a414
farishariri <fhariri56@gmail.com>
2022-06-06 23:06:17 +0300

    test

---
+++ script.js
@@ -0,0 +1,34 @@
+const canvas = document.querySelector('canvas')
+
+const c = canvas.getContext('2d')
+
+console.log(c)
+
+class Player {
+    constructor(){
+        this.position = {
+            x: 100,
+            y: 100
+        }
+        this.velocity = {
+            x: 0,
+            y: 1
+        }
+
+        this.width = 100
+        this.height = 100
+    } 
+    
+    draw() {
+        c.fillRect(this.position.x, this.position.y, this.width, this.height)
+    }
+}
+
+const player = new Player()
+
+//
+function animate() {
+    requestAnimationFrame(animate)
+    c.clearRect(0, 0, canvas.width, canvas.hight)
+    PLayer.update()
+}
