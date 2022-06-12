e5bd0ac27245a462ada99c7a22483a35e19798bf
farishariri <fhariri56@gmail.com>
2022-06-13 01:15:57 +0300

    Player Creation Done

--- script.js
+++ script.js
@@ -1,8 +1,17 @@
+// storing the HTML canvas element in a "constant" called canvas
+
 const canvas = document.querySelector('canvas')
 
 const c = canvas.getContext('2d')
 
+// changing canvas properties so it fits the whole screen
+
+canvas.width = window.innerWidth
+canvas.height = window.innerHeight
+
 console.log(c)
+
+// Player Creation
 
 class Player {
     constructor() {
@@ -10,27 +19,21 @@ class Player {
             x: 100,
             y: 100
         }
-        this.velocity = {
-            x: 0,
-            y: 1
-        }
 
-        this.width = 100
-        this.height = 100
+        this.width = 30
+        this.height = 30
     }
 
     draw() {
+        c.fillStyle = 'red'
         c.fillRect(this.position.x, this.position.y, this.width, this.height)
     }
 
 }
 
+// Creating a "player" object and displaying them on the screen
+
 const player = new Player()
 player.draw()
 
-//
-function animate() {
-    requestAnimationFrame(animate)
-    c.clearRect(0, 0, canvas.width, canvas.hight)
-    PLayer.update()
-}
+// Gravity
