42ec45b0fff55788da3691dbf5434347ca06348d
Mokuhito <k.alfaidi2@gmail.com>
2022-06-13 12:43:33 +0300

    gravity

--- script.js
+++ script.js
@@ -11,6 +11,10 @@ canvas.height = window.innerHeight
 
 console.log(c)
 
+//gravity speed
+
+const Gravity = 0.5
+
 // Player Creation
 
 class Player {
@@ -19,11 +23,23 @@ class Player {
             x: 100,
             y: 100
         }
-
+        this.velocity = {
+            x: 0,
+            y:1
+        }
         this.width = 30
         this.height = 30
     }
 
+    upddate() {
+        this.draw()
+        this.position.y += this.velocity.y
+        this.velocity.y += Gravity
+        if (this.position.y +this.hight + this.velocity.y <= canvas.hight)
+        this.velocity.y += Gravity
+        else this.velocity.y = 0
+    }
+ 
     draw() {
         c.fillStyle = 'red'
         c.fillRect(this.position.x, this.position.y, this.width, this.height)
@@ -37,3 +53,9 @@ const player = new Player()
 player.draw()
 
 // Gravity
+
+function animate() {
+    requestAnimationFrame(animate)
+    c.clearRect(0, 0, canvas.width, canvas.hight)
+    player.upddate()
+}
