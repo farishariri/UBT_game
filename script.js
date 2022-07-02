7df39dcfb01939e3c492a78d871b8cd72fa875d7
farishariri <fhariri56@gmail.com>
2022-07-02 20:43:27 +0300

    Gravity Fix

--- script.js
+++ script.js
@@ -9,8 +9,6 @@ const c = canvas.getContext('2d')
 canvas.width = window.innerWidth
 canvas.height = window.innerHeight
 
-console.log(c)
-
 //gravity speed
 
 const Gravity = 0.5
@@ -25,37 +23,44 @@ class Player {
         }
         this.velocity = {
             x: 0,
-            y:1
+            y: 1
         }
         this.width = 30
         this.height = 30
     }
 
-    upddate() {
-        this.draw()
-        this.position.y += this.velocity.y
-        this.velocity.y += Gravity
-        if (this.position.y +this.hight + this.velocity.y <= canvas.hight)
-        this.velocity.y += Gravity
-        else this.velocity.y = 0
-    }
- 
     draw() {
         c.fillStyle = 'red'
         c.fillRect(this.position.x, this.position.y, this.width, this.height)
     }
 
+    update() {
+        this.draw()
+
+        this.position.y += this.velocity.y
+        this.velocity.y += Gravity
+
+        if (this.position.y + this.height + this.velocity.y <= canvas.height)
+            this.velocity.y += Gravity
+        else this.velocity.y = 0
+    }
+
 }
+
+
 
 // Creating a "player" object and displaying them on the screen
 
 const player = new Player()
-player.draw()
+
 
 // Gravity
 
 function animate() {
     requestAnimationFrame(animate)
-    c.clearRect(0, 0, canvas.width, canvas.hight)
-    player.upddate()
+    c.clearRect(0, 0, canvas.width, canvas.height)
+    player.update()
+
 }
+
+animate()
