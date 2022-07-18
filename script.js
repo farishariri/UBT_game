40fc7ea17d9c7ed1e7d052f1fd6cc82f568a1b36
Mokuhito <k.alfaidi2@gmail.com>
2022-07-18 15:30:13 +0300

    Platform

--- script.js
+++ script.js
@@ -50,11 +50,33 @@ class Player {
 
 }
 
+// platform class
+// x and y is where the platform is placed
+
+class Platform {
+    constructor() {
+        this.position = {
+            x: 500,
+            y: 800
+        }
+
+        this.width = 200
+        this.height = 20
+    }
+
+    draw() {
+        c.fillStyle = 'blue'
+        c.fillRect(this.position.x, this.position.y, this.width, this.height)
+    }
+}
+
 
 
 // Creating a "player" object and displaying them on the screen
+// creating a "platform" object and displaying them on the screen
 
 const player = new Player()
+const platform = new Platform()
 
 const keys = {
     right: {
@@ -72,12 +94,19 @@ function animate() {
     requestAnimationFrame(animate)
     c.clearRect(0, 0, canvas.width, canvas.height)
     player.update()
+    platform.draw()
 
     if (keys.right.pressed) {
         player.velocity.x = 5
     } else if (keys.left.pressed) {
         player.velocity.x = -5
     } else player.velocity.x = 0
+
+    //platform collision detection
+    if (player.position.y + player.height <= platform.position.y && player.position.y + player.height + player.velocity.y >= platform.position.y && player.position.x + player.width >= platform.position.x && player.position.x <= platform.position.x + platform.width
+        ) {
+        player.velocity.y = 0
+    }
 
 }
 
